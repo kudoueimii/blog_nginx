d4b9e653a0fee74c3b88eed34990973d7a4c6371
bargholz eimii <818_77_@ezweb.ne.jp>
2022-11-11 15:53:29 +0900

    root

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   resources :blogs do
     root 'blogs#index'
-  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
